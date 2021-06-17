fc57b66e76f8257c13a688c6d015164b28570006
SakriyaDangol <sakriyadangol003@gmail.com>
2021-06-17 17:51:38 +0545

    second commit

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const heading = document.createElement('h2');
 document.body.appendChild(heading);
 heading.innerHTML = `CatPhotoApp`;
 
-// const anchor = document.createElement('a');
-// anchor.innerHTML = `cat photos.`;
-// document.para1.appendChild(anchor);
-
 const para1 = document.createElement('p');
 document.body.appendChild(para1);
-para1.innerHTML = `Click here to view more cat photos.` ;
+para1.innerHTML = `Click here to view more` + ' ';
+
+const anchor = document.createElement('a');
+anchor.innerHTML = `cat photos.`;
+anchor.setAttribute('href','https://www.google.com/');
+para1.appendChild(anchor);
 
 const para2 = document.createElement('p');
 document.body.appendChild(para2);
@@ -18,17 +19,17 @@ const uList = document.createElement('ul');
 document.body.appendChild(uList);
 
 const ulLi = document.createElement('li');
-
-var list = [`cat nip`, `laser pointer`, `lasagna`];
+const ulText = ['asdasdasd'];
+uList.appendChild(ulLi);
+ulLi.innerHTML = ['asdasd', 'asdasd'];
 
 // for (let i=0; i<list.length; i++){
-    
+//     var list = [`cat nip`, `laser pointer`, `lasagna`];
 //     if(list[i]){
-//         ulLi.innerHTML = list[i];
+//         input = list[i];
 //     }
+//     uList.appendChild(input);
 // }     
-
-uList.appendChild(ulLi);
 
 const para3 = document.createElement('p');
 document.body.appendChild(para3);
